a74888ebc8a88b3695f21c903cf13c5726a897f4
Cara M Locke <81659057+caralocke@users.noreply.github.com>
2021-08-25 10:54:29 -0400

    Added returning the user_id upon login.

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -32,6 +32,7 @@ router.post('/login', (req, res) => {
         if (user && bcrypt.compareSync(password, user.password)) {
           const token = buildToken(user)
           res.status(200).json({
+            user_id: user.user_id,
             message: `welcome, ${username}`,
             token: token
           })
